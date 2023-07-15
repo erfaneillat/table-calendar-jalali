7cf042ae9209fe0912ca5132fe428e30b8d82b42
erfan eillat <erfaneillat@gmail.com>
2023-07-15 20:48:48 +0330

    fix exports

--- lib/jalali_table_calendar.dart
+++ lib/jalali_table_calendar.dart
@@ -1,4 +1,4 @@
 library jalali_table_calendar;
 
 export 'package:table_calendar_jalali/src/extensions/string.dart';
-export 'package:table_calendar_jalali/table_calendar_jalali.dart';
+export 'package:table_calendar_jalali/src/table_calendar_jalali.dart';
--- test/table_calendar_jalali_test.dart
+++ test/table_calendar_jalali_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:table_calendar_jalali/table_calendar_jalali.dart';
+import 'package:table_calendar_jalali/src/table_calendar_jalali.dart';
 
 void main() {}
