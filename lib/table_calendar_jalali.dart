282689c8a245385c2551e1b0b94a026551edbcce
erfan eillat <erfaneillat@gmail.com>
2023-07-15 20:57:08 +0330

    fix table_calendar_jalali

--- lib/table_calendar_jalali.dart
+++ lib/table_calendar_jalali.dart
@@ -1,4 +1,4 @@
-library jalali_table_calendar;
+library table_calendar_jalali;
 
 export 'package:table_calendar_jalali/src/extensions/string.dart';
 export 'package:table_calendar_jalali/src/table_calendar_jalali.dart';
